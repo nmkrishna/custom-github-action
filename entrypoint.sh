6cf2c2c456bfc60d8accd417bcda68afc8bad62b
Chaitanyakusumba <chaitanya@digitaldots.ai>
2022-01-06 13:04:00 +0530

    My first action is ready

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+case "$1" in
+    java)
+        echo "java"
+        ;;
+    python)
+        echo "python"
+        ;;
+    node)
+        npm install -g npm@7.5.6
+        npm install 
+        license_finder report --format html > license.html
+        python3 parser.py > license.csv
+        ;;
+esac
