053cb48aacf4ba63567cfed3f4ff2e69054f912f
Chaitanyakusumba <chaitanya@digitaldots.ai>
2022-01-06 17:46:36 +0530

    updated entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 case "$1" in
     java)
-        echo "java"
+        cp -rf . .
+        apt update -y
+        apt install ruby-dev -y
+        export PATH=/usr/share/rvm/rubies/ruby-2.7.1/bin:$PATH
+        gem install license_finder
+        license_finder report --maven-include-groups=true --format html > license.html
+        python3 parser.py > license.csv
         ;;
     python)
-        echo "python"
+        cp -rf . .
+        apt update -y
+        apt install ruby-dev -y
+        export PATH=/usr/share/rvm/rubies/ruby-2.7.1/bin:$PATH
+        pip3 install -r requirement.txt
+        license_finder report --python-version=3 --format html > license.html
+        python3 parser.py > license.csv
         ;;
     node)
         cp -rf . .
