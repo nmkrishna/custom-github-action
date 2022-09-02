da99fd12e3af0265d6da588a04379d384af5b421
chaitanyakusumba <79130551+chaitanyakusumba@users.noreply.github.com>
2022-09-02 10:55:21 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,9 @@ case "$1" in
         ;;
     python)
         cp -rf . .
-        apt update -y
-        apt install ruby-dev -y
-        export PATH=/usr/share/rvm/rubies/ruby-2.7.1/bin:$PATH
+        apt-get update -y
+        apt-get install ruby -y
+        #export PATH=/usr/share/rvm/rubies/ruby-2.7.1/bin:$PATH
         gem install license_finder
         pip3 install virtualenv
         python3 -m virtualenv MyEnv
